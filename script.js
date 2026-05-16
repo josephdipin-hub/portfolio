f450c538371f1d45f1b5adae927ebe3e0b9336c1
josephdipin-hub <joseph.dipin@gmail.com>
2026-05-16 16:13:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -331,12 +331,12 @@ function initEnlargerBg() {
   pgScene.add(new THREE.HemisphereLight(0x2a0010, 0x080015, 2.0));
 
   
- /* var fill = new THREE.PointLight(0xff1144, 5, 60, 1.0);
-fill.position.set(6, 16, 5);*/
+  var fill = new THREE.PointLight(0xff1144, 5, 60, 1.0);
+fill.position.set(6, 26, 5);
 
-  var rim = new THREE.PointLight(0x880033, 6, 20, 1.8);
+/*  var rim = new THREE.PointLight(0x880033, 6, 20, 1.8);
   rim.position.set(9, 15, -5);
-  pgScene.add(rim);
+  pgScene.add(rim);*/
 
   /* Load model */
   var loader = new THREE.GLTFLoader();
